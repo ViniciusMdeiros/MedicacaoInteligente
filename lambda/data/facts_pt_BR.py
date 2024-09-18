66f620a83d0df63e5d63fce8b242418a85790f54
bruno <brunob.marques@hotmail.com>
2024-09-18 17:56:34 -0300

    Ajustes em detalhes

---
+++ lambda/data/facts_pt_BR.py
@@ -0,0 +1,96 @@
+data = [
+    # Medicamento - Dor de cabeça
+  "1": [
+    {
+      "nome": "Paracetamol",
+      "marca": "Tylenol",
+      "indicacao": "Alívio de dores leves a moderadas, incluindo dores de cabeça"
+    },
+    {
+      "nome": "Dipirona Sódica",
+      "marca": "Novalgina",
+      "indicacao": "Analgésico e antitérmico utilizado para tratar dores de cabeça"
+    },
+    {
+      "nome": "Ácido Acetilsalicílico",
+      "marca": "Aspirina",
+      "indicacao": "Alívio de dores leves e moderadas, como dores de cabeça tensionais e enxaquecas leves"
+    },
+    {
+      "nome": "Ibuprofeno",
+      "marca": "Advil, Motrin",
+      "indicacao": "Anti-inflamatório não esteroidal utilizado para tratar dores de cabeça causadas por inflamação"
+    },
+    {
+      "nome": "Naproxeno",
+      "marca": "Flanax, Naprosyn",
+      "indicacao": "Anti-inflamatório não esteroidal utilizado no tratamento de dores de cabeça, especialmente enxaquecas"
+    }
+  ],
+  #Medicamento - Pressão Alta
+  "2": [
+    {
+      "nome": "Hidroclorotiazida",
+      "indicacao": "Diurético tiazídico amplamente utilizado para reduzir a pressão arterial ao eliminar excesso de sal e água"
+    },
+    {
+      "nome": "Furosemida",
+      "marca": "Lasix",
+      "indicacao": "Diurético de alça utilizado para hipertensão e retenção de líquidos"
+    },
+    {
+      "nome": "Losartana",
+      "marca": "Cozaar",
+      "indicacao": "Bloqueador do receptor de angiotensina II, amplamente utilizado no Brasil para tratar hipertensão"
+    },
+    {
+      "nome": "Valsartana",
+      "marca": "Diovan",
+      "indicacao": "Comumente usado para reduzir a pressão arterial ao bloquear os efeitos da angiotensina II"
+    },
+    {
+      "nome": "Captopril",
+      "indicacao": "Inibidor da ECA bastante utilizado para relaxar os vasos sanguíneos e reduzir a pressão arterial"
+    },
+    {
+      "nome": "Enalapril",
+      "marca": "Renitec",
+      "indicacao": "Comumente usado para tratar hipertensão e insuficiência cardíaca ao dilatar os vasos sanguíneos"
+    },
+     {
+      "nome": "Atenolol",
+      "indicacao": "Betabloqueador amplamente prescrito para tratar hipertensão e reduzir o risco de complicações cardíacas"
+    },
+    {
+      "nome": "Propranolol",
+      "indicacao": "Usado para tratar hipertensão, angina e arritmias"
+    },
+    {
+      "nome": "Anlodipino",
+      "marca": "Norvasc",
+      "indicacao": "Comumente usado no Brasil para relaxar os vasos sanguíneos e melhorar o fluxo sanguíneo"
+    }
+  ],
+  #Medicamento - Gripe
+  "3": [
+    {
+      "nome": "Antigripal",
+      "marca": "Benegrip",
+      "indicacao": "Combinado de paracetamol, cafeína e anti-histamínico para alívio de sintomas de gripe"
+    },
+    {
+      "nome": "Multigrip",
+      "indicacao": "Combinado de analgésico, anti-histamínico e descongestionante nasal para tratar sintomas de gripe"
+    },
+    {
+      "nome": "Resfenol",
+      "indicacao": "Antigripal que combina analgésico, antitérmico e descongestionante para aliviar sintomas gripais"
+    }
+  ],
+]
+
+SKILL_NAME = "Medicação Inteligente"
+GET_FACT_MESSAGE = "Você deve tomar:"
+HELP_MESSAGE = "Você pode dizer quais remédios eu devo tomar durante a manhã ou perguntar algo sobre a sua rotina de remédios..."
+HELP_REPROMPT = "Em que posso ajudá-lo?"
+STOP_MESSAGE = "Tchau, obrigado por utilizar o Medicação Inteligente! Até mais!"
